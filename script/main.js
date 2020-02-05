43ea209fcc0d033dd4b38b1bc982ed60439e2682
new-reality-design <54451894+new-reality-design@users.noreply.github.com>
2020-02-05 19:06:07 +0000

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,12 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+  // speed: 400,
+  // spaceBetween: 100
+  pagination: {
+    el: '.projects-pagination',
+    type: 'bullets',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true
+  },
+});
